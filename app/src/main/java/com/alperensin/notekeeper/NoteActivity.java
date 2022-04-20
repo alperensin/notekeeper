64c5aaeeedd8c33f678630eef420c5bd9211c821
André Luiz Perensin <andreluizperensin@gmail.com>
2022-04-20 00:24:58 -0300

    Save original note values

--- app/src/main/java/com/alperensin/notekeeper/NoteActivity.java
+++ app/src/main/java/com/alperensin/notekeeper/NoteActivity.java
@@ -27,6 +27,9 @@ public class NoteActivity extends AppCompatActivity {
     private boolean mIsNewNote;
     private boolean mIsCancelling;
     private int mNotePosition;
+    private String mOriginalNoteCourseId;
+    private String mOriginalNoteTitle;
+    private String mOriginalNoteText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +45,7 @@ public class NoteActivity extends AppCompatActivity {
         mSpinnerCourses.setAdapter(adapterCourses);
         
         readDisplayStateValues();
+        saveOriginalNoteValues();
 
         mTextNoteTitle = findViewById(R.id.text_note_title);
         mTextNoteText = findViewById(R.id.text_note_text);
@@ -50,15 +54,34 @@ public class NoteActivity extends AppCompatActivity {
             displayNote(mSpinnerCourses, mTextNoteTitle, mTextNoteText);
     }
 
+    private void saveOriginalNoteValues() {
+        if (mIsNewNote)
+            return;
+
+        mOriginalNoteCourseId = mNote.getCourse().getCourseId();
+        mOriginalNoteTitle = mNote.getTitle();
+        mOriginalNoteText = mNote.getText();
+    }
+
     @Override
     protected void onPause() {
         super.onPause();
         if (mIsCancelling) {
-            if (mIsNewNote)
+            if (mIsNewNote) {
                 DataManager.getInstance().removeNote(mNotePosition);
+            } else {
+                storePreviousNoteValues();
+            }
         } else {
             saveNote();
         }
+    }
+
+    private void storePreviousNoteValues() {
+        CourseInfo course = DataManager.getInstance().getCourse(mOriginalNoteCourseId);
+        mNote.setCourse(course);
+        mNote.setTitle(mOriginalNoteTitle);
+        mNote.setText(mOriginalNoteText);
     }
 
     private void saveNote() {
